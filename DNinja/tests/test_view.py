b56b0a1e585bcedf4f1b37c1e759a545c5bd9b09
yongheon-Lee <dydgjs778@gmail.com>
2022-02-11 20:49:28 +0900

    test case code commit

---
+++ DNinja/tests/test_view.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+
+class TestView(TestCase):
+
+    def test_add_view(self) -> None:
+        result = self.client.get("/api/add", {"a": 1, "b": 3})
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result.json(), {"result": 4})
--- DNinja/urls.py
+++ DNinja/urls.py
@@ -1,16 +1,19 @@
+from typing import Dict
+
 from django.contrib import admin
+from django.http import HttpRequest
 from django.urls import path
-from ninja import NinjaAPI # ninja 객체 import
+from ninja import NinjaAPI  # ninja 객체 import
 
-api = NinjaAPI()           # 객체 생성
+api = NinjaAPI()  # 객체 생성
 
 
-@api.get("/add")           # view 함수를 데코레이터(@)로 감싸서 사용
-def add(request, a: int, b: int):
+@api.get("/add")  # view 함수를 데코레이터(@)로 감싸서 사용
+def add(request: HttpRequest, a: int, b: int) -> Dict[str, int]:
     return {"result": a + b}
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", api.urls),  #
+    path("api/", api.urls),
 ]
